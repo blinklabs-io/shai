4377f199c88c8f7ebe0a40d928e5813923488c78
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-06-29 18:03:08 -0400

    refactor: move geniusyield parser to dex package; real deployment identifiers

--- dex/geniusyield_test.go
+++ dex/geniusyield_test.go
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package oracle
+package dex
 
 import (
 	"testing"
 	"time"
 
 	"github.com/blinklabs-io/gouroboros/cbor"
-	"github.com/blinklabs-io/shai/internal/oracle/geniusyield"
+	"github.com/blinklabs-io/shai/dex/geniusyield"
 )
 
 func TestNewGeniusYieldParser(t *testing.T) {
